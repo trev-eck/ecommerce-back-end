d79fb97511dcafae2bcdcfb1cf0133b0bc993b14
Trevor Eckberg <trevor.eckberg@gmail.com>
2021-04-29 14:49:56 -0700

    fixed delete category route

--- models/index.js
+++ models/index.js
@@ -4,21 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+
 Product.belongsTo(Category, {
 foreignKey: 'category_id',
+onDelete: 'CASCADE',
 });
-// Categories have many Products
+
 Category.hasMany(Product, {
 foreignKey: 'category_id',
 });
-// Products belongToMany Tags (through ProductTag)
+
 Product.belongsToMany(Tag, {
 through: {
   model: ProductTag,
 }
 });
-// Tags belongToMany Products (through ProductTag)
+
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
